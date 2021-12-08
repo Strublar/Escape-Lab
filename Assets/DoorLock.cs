ef04d11178586805ae22e040d1f1d1cdfead8223
Strublar <daluzmatteo@gmail.com>
2021-12-08 11:39:54 +0100

    Key interaction + door

---
+++ Assets/DoorLock.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class DoorLock : MonoBehaviour
+{
+    [SerializeField] private Rigidbody doorRb;
+
+    public void Start()
+    {
+        doorRb.constraints = RigidbodyConstraints.FreezeAll;
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("object entered doorlock");
+        if (other.transform.tag == "key")
+        {
+            Debug.Log("Key entered doorlock");
+            doorRb.constraints = RigidbodyConstraints.FreezePosition |
+                RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        }
+    }
+}
